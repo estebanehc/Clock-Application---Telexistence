c5e7f977aa28ac141ccab762db74a95018ce0869
Esteban Herrera <e.herreraclaveria@gmail.com>
2025-05-03 12:47:34 +0900

    clock refactoring ok

---
+++ Assets/Scripts/Core/Interfaces/IClockView.cs
@@ -0,0 +1,4 @@
+public interface IClockView
+{
+    void UpdateTimeDisplay(string time);
+}
--- Assets/Scripts/Installer/ProjectInstaller.cs
+++ Assets/Scripts/Installer/ProjectInstaller.cs
@@ -4,11 +4,24 @@ using Zenject;
 public class ProjectInstaller : MonoInstaller
 {
     [SerializeField] private StopwatchView stopwatchView;
+    [SerializeField] private ClockView clockView;
 
     public override void InstallBindings()
     {
-        Container.Bind<IClockService>().To<ClockService>().AsSingle();
-        Container.Bind<ClockModel>().AsSingle();
+        Container.Bind<IClockView>()
+            .FromInstance(clockView)
+            .AsSingle();
+
+        Container.Bind<IClockService>()
+            .To<ClockService>()
+            .AsSingle();
+
+        Container.Bind<ClockPresenter>()
+            .AsSingle()
+            .NonLazy();
+
+        Container.Bind<ClockModel>()
+            .AsSingle();
 
         Container.Bind<ITimerService>().To<TimerService>().AsSingle();
         Container.Bind<TimerModel>().AsSingle();
